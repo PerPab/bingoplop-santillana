528dc5929e59d3e846669be502d2b7464d639b3d
Pablo <percara.pablo@gmail.com>
2024-07-09 17:57:29 -0300

    version final

--- script/main.js
+++ script/main.js
@@ -4,16 +4,19 @@ import { obtenerNumeroAleatorio, animacionFlip, recargarLista, cargarLista } fro
 let carta = document.getElementById('carta');
 let btn_mostrar = document.getElementById('btn-girar');
 let btn_reinicio = document.getElementById('btn-reiniciar');
+let caja_btn = document.getElementById('contenedor-btn');
 
 cargarLista();
 
 function comienzo() {
+    caja_btn.classList.add('invisible');
     btn_mostrar.setAttribute('disabled', 'true')
     carta.innerHTML = '';
     let numeroAleatorio = obtenerNumeroAleatorio();
     animacionFlip(numeroAleatorio);
     setTimeout(() => {
         btn_mostrar.removeAttribute('disabled')
+        caja_btn.classList.remove('invisible');
     }, 3000);
 
 }
